fd912e1aa95a2f83e8e5180d0d30f3234689743d
dianaenrique <136309694+dianaenrique@users.noreply.github.com>
2023-06-12 17:49:27 +0800

    Add files via upload

---
+++ css.html/JS script.js
@@ -0,0 +1,29 @@
+//TODO<---------------------------------Auto Scrolling NAVBAR---------------------------------->
+const section=document.querySelectorAll('section')
+const navLink=document.querySelectorAll('header nav a')
+window.onscroll=()=>{
+    section.forEach(sec=>{
+        let top=window.scrollY;
+        let offset=sec.offsetTop - 150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id')
+        if(top>=offset && top< offset + height){
+            navLink.forEach(links=>{
+                links.classlis.remove('active')
+                document.querySelector('header nav a[href*='+id+']').classList.add('active')
+            })
+        }
+    })
+//TODO<---------------------------------STICKY NAVBAR---------------------------------->
+var header=document.querySelector('header')
+header.classList.toggle("sticky",window.screenY>100)
+menubar.classList.remove('bx-x')
+navbar.classList.remove('active')
+};
+//TODO<---------------------------------TOGGLE CHANGER---------------------------------->
+let menubar=document.querySelector('#menu')
+let navbar=document.querySelector('.navbar')
+menubar.onclick= ()=>{
+    menubar.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
